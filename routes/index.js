39b7cce75f6355dcf34102c7b70e356214dae5bb
Brad <bradleykigsley2@gmail.com>
2020-10-11 14:44:43 +0300

    feat: finish validation logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const emailValidator = require('deep-email-validator');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+async function isEmailValid(email) {
+  return emailValidator.validate(email, {})
+}
+
+router.post('/register', async function(req, res) {
+  const {email, password} = req.body;
+
+  if (!email || !password){
+    return res.status(400).send({
+      message: "Email or password missing."
+    })
+  }
+
+  const {valid, reason, validators} = await isEmailValid(email);
+
+  if (valid) return res.send({message: "OK"});
+
+  return res.status(400).send({
+    message: "Please provide a valid email address.",
+    reason: validators[reason].reason
+  })
+
 });
 
 module.exports = router;
